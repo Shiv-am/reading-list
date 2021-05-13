a8f19d32506417a4016812316bd8d612a69045fd
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-05-13 13:24:31 +0530

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React,{useContext} from 'react'
+import { BookContext } from '../contexts/BookContext'
+
+const Navbar = () => {
+    const {books} = useContext(BookContext)
+    return(
+        <div className="navbar">
+            <h1>Reading List</h1>
+            <p>You have {books.length} to go through.</p>
+        </div>
+    )
+}
+export default Navbar
