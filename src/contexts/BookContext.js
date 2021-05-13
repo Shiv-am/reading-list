e134e527bd58ac28a3518df7f18043ef3af227c0
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-05-13 14:41:21 +0530

    updates

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,5 +1,5 @@
 import React, { createContext,useState } from "react"
-import uuid from 'uuid/v1'
+import {v1 as uuid} from "uuid";
 export const BookContext = createContext();
 
 const BookContextProvider = (props) =>{
@@ -7,14 +7,14 @@ const BookContextProvider = (props) =>{
             {title:"any book",author:"shivam",id:1},
             {title:"any book",author:"bansal",id:1}
         ])
-        const addBook =(title,auhtor)=>{
+        const addBook =(title,author)=>{
             setbooks([...books, {title,author,id: uuid()}])
         }
         const removeBook = (id) =>{
             setbooks(books.filter(book => book.id!== id))
         }
         return(
-            <BookContextProvider>
+            <BookContextProvider value={{books,addBook,removeBook}}>
                 {props.children}
             </BookContextProvider>
         )
